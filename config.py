821f99ae04d78baec2d8027464c4931a8ac164a1
Ashutosh <aashutosh1081@gmail.com>
2026-07-11 01:14:55 +0530

    Support local MySQL and Render PostgreSQL

--- config.py
+++ config.py
@@ -7,6 +7,17 @@ class Config:
 
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    if DATABASE_URL:
+        SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    else:
+        SQLALCHEMY_DATABASE_URI = (
+            f"mysql+pymysql://{os.getenv('DB_USERNAME')}:"
+            f"{os.getenv('DB_PASSWORD')}@"
+            f"{os.getenv('DB_HOST')}:"
+            f"{os.getenv('DB_PORT')}/"
+            f"{os.getenv('DB_NAME')}"
+        )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
